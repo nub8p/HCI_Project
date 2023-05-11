e45580033191815d86b81cef4778d063132610d4
Anthony <anthony.deleener@student.vinci.be>
2023-05-11 16:26:40 +0900

    html injections from js for emotions

---
+++ components/playlistPage.js
@@ -0,0 +1,9 @@
+  let emotion_1_value = "null";
+  let emotion_2_value = "null";
+  
+  let emotion_1 = document.querySelector("#emotion_1");
+  emotion_1.innerHTML = emotion_1_value += ` the sin,`;
+
+  let emotion_2 = document.querySelector("#emotion_2");
+  emotion_2.innerHTML = emotion_2_value += ` the sinner`;
+
