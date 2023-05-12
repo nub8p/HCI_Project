904ac9771bbd06977d19a598914f5c8cc6d9e695
Anthony <anthony.deleener@student.vinci.be>
2023-05-12 20:56:11 +0900

    selectPage pull + norms standardisation

--- components/playlistPage.js
+++ components/playlistPage.js
@@ -2,8 +2,8 @@
   let emotion_2_value = "null";
   
   let emotion_1 = document.querySelector("#emotion_1");
-  emotion_1.innerHTML = emotion_1_value += ` the sin,`;
+  emotion_1.innerHTML = `<button id="whiteBtn">` + emotion_1_value + `</button> the sin,`;
 
   let emotion_2 = document.querySelector("#emotion_2");
-  emotion_2.innerHTML = emotion_2_value += ` the sinner`;
+  emotion_2.innerHTML = `<button id="whiteBtn">` + emotion_2_value + `</button> the sinner`;
 
