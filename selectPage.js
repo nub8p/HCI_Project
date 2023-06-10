c13ed0f80c994b931b896baf7192c2788d30d648
unknown <aks152312@gmail.com>
2023-06-10 22:43:24 +0900

    doing mainpage

--- selectPage.js
+++ selectPage.js
@@ -35,10 +35,29 @@ const blackImg = [
   "angry3_black.png",
 ];
 
+const hoverImg = [
+  "happy1_hover.png",
+  "happy2_hover.png",
+  "happy3_hover.png",
+  "love1_hover.png",
+  "love2_hover.png",
+  "love3_hover.png",
+  "fear1_hover.png",
+  "fear2_hover.png",
+  "fear3_hover.png",
+  "sad1_hover.png",
+  "sad2_hover.png",
+  "sad3_hover.png",
+  "angry1_hover.png",
+  "angry2_hover.png",
+  "angry3_hover.png",
+];
+
 const currMoodDefault = "# current mood";
 const nextMoodDefault = "# mood I want";
 
 const emoji = document.querySelectorAll(".emoji");
+let selectedList = [];
 let selected = 0;
 let currMood = "";
 let nextMood = "";
@@ -51,21 +70,57 @@ const nextMoodImg = document.querySelector("#nextMoodImg");
 const defaultDiv = document.querySelector(".defaultDiv");
 const selectedDiv = document.querySelector(".selectedDiv");
 
+const state = Array.from({ length: 15 }, () => 0);
+
 for (let i = 0; i < emoji.length; i++) {
   emoji[i].addEventListener("click", changePic);
+  emoji[i].addEventListener("mouseover", mouseover);
+  emoji[i].addEventListener("mouseout", mouseleave);
+}
+
+function StopShaking() {
+  for (let i = 0; i < emoji.length; i++) {
+    emoji[i].setAttribute("style", "animation-play-state: paused");
+  }
+}
+
+function RestartShaking() {
+  for (let i = 0; i < emoji.length; i++) {
+    if (selectedList[0] !== emoji[i]) {
+      emoji[i].setAttribute("style", "animation-play-state: running");
+    }
+  }
+}
+
+function mouseover(e) {
+  const idx = e.target.dataset.idx;
+  if (state[idx] === 0) {
+    e.target.setAttribute("src", "./images/" + hoverImg[idx]);
+  }
+}
+
+function mouseleave(e) {
+  const idx = e.target.dataset.idx;
+  if (state[idx] === 0) {
+    e.target.setAttribute("src", "./images/" + colorImg[idx]);
+  }
 }
 
 function changePic() {
   let picture = this.getAttribute("src").split("/")[2];
-  let i = colorImg.findIndex((i) => i === picture);
+  console.log(picture);
+  let i = hoverImg.findIndex((i) => i === picture);
   if (i >= 0) {
     if (selected < 2) {
+      selectedList.push(this);
       this.setAttribute("src", "./images/" + blackImg[i]);
+      this.setAttribute("style", "animation-play-state: paused");
       selected += 1;
       if (selected === 1) {
         currMood = this.dataset["tag"];
         currMoodText.textContent = "# " + currMood;
-        let currSrc = "./images/" + picture;
+        let currSrc = "./images/" + colorImg[i];
+        state[i] = 1;
         currMoodImg.src = currSrc;
         currMoodImg.style.display = "inline-block";
         window.localStorage.setItem("currMood", currMood);
@@ -73,33 +128,42 @@ function changePic() {
       } else if (selected === 2) {
         nextMood = this.dataset["tag"];
         nextMoodText.textContent = "# " + nextMood;
-        let nextSrc = "./images/" + picture;
+        let nextSrc = "./images/" + colorImg[i];
+        state[i] = 1;
         nextMoodImg.src = nextSrc;
         nextMoodImg.style.display = "inline-block";
         defaultDiv.style.display = "none";
         selectedDiv.style.display = "inline-block";
         window.localStorage.setItem("nextMood", nextMood);
         window.localStorage.setItem("nextImg", nextSrc);
+        StopShaking();
       }
     }
   } else {
     i = blackImg.findIndex((i) => i === picture);
     this.setAttribute("src", "./images/" + colorImg[i]);
+    this.setAttribute("style", "animation-play-state: running");
     selected -= 1;
+    selectedList = selectedList.filter((e) => {
+      return e !== this;
+    });
     if (selected === 1) {
       nextMood = "";
       nextMoodText.textContent = nextMoodDefault;
       nextMoodImg.src = "";
+      state[i] = 0;
       nextMoodImg.style.display = "none";
       defaultDiv.setAttribute("transform", "none");
       defaultDiv.style.display = "inline-block";
       selectedDiv.style.display = "none";
       window.localStorage.removeItem("nextMood");
       window.localStorage.removeItem("nextImg");
+      RestartShaking();
     } else if (selected === 0) {
       currMood = "";
       currMoodText.textContent = currMoodDefault;
       currMoodImg.src = "";
+      state[i] = 0;
       currMoodImg.style.display = "none";
       window.localStorage.removeItem("currMood");
       window.localStorage.removeItem("currImg");
